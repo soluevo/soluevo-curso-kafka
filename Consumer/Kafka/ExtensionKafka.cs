70ec789b20fd1bffd8c07d83ca9abe0e02a94520
rlanhellas <ronaldo.lanhellas@soluevo.com.br>
2020-11-17 10:45:43 -0300

    Criando projeto consumer

---
+++ Consumer/Kafka/ExtensionKafka.cs
@@ -0,0 +1,28 @@
+using System.Net;
+using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Consumer.Kafka
+{
+    public static class ExtensionKafka
+    {
+        public static void AddKafka(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton(c => AddConsumerAnima(configuration));
+        }
+
+        private static IConsumer<Ignore, string> AddConsumerAnima(IConfiguration configuration)
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = configuration["Kafka:Servers"],
+                GroupId = configuration["Kafka:ConsumerGroupId"],
+                ClientId = configuration["Kafka:ClientId"] + "-" + Dns.GetHostName(),
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+            
+            return new ConsumerBuilder<Ignore, string>(config).Build();
+        }
+    }
+}
---
+++ Consumer/Worker.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Consumer
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IConsumer<Ignore, string> _consumer;
+        private readonly IConfiguration _configuration;
+
+        public Worker(ILogger<Worker> logger, IConsumer<Ignore, string> consumer, IConfiguration configuration)
+        {
+            _logger = logger;
+            _consumer = consumer;
+            _configuration = configuration;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _consumer.Subscribe(_configuration["Kafka:TopicAnima"]);
+            try
+            {
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    _logger.LogInformation("Worker Consumer running at: {time}", DateTimeOffset.Now);
+                    try
+                    {
+                        var result = _consumer.Consume(stoppingToken);
+                        _logger.LogInformation("Result {}", result.Message.Value);
+                        await Task.Delay(1000, stoppingToken);
+                    }
+                    catch (ConsumeException e)
+                    {
+                        _logger.LogError(e, "Error consuming");
+                    }
+                }
+            }
+            finally
+            {
+                _consumer.Close();    
+            }
+        }
+    }
+}
